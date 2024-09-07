7646197466f56510239e12959db7b1dad5ca10ae
officialBox <yaseerbostbox@gmail.com>
2024-09-07 10:26:47 +0530

    reduce advance concept before learning

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,50 +1,50 @@
-import { v2 as cloudinary } from 'cloudinary';
-import fs from 'fs';
-import dotenv from "dotenv"
+// import { v2 as cloudinary } from 'cloudinary';
+// import fs from 'fs';
+// import dotenv from "dotenv"
 
-dotenv.config()
-// confiqure cloudinary
+// dotenv.config()
+// // confiqure cloudinary
 
-cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
-});
+// cloudinary.config({
+//     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//     api_key: process.env.CLOUDINARY_API_KEY,
+//     api_secret: process.env.CLOUDINARY_API_SECRET
+// });
 
-const uploadOnCloudinary = async (localFilePath) => {
-    try {
-        // console.log("Cloudinary Confiq:",{
-        //     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-        //     api_key: process.env.CLOUDINARY_API_KEY,
-        //     api_secret: process.env.CLOUDINARY_API_SECRET
-        // })
-        if(!localFilePath) return null
-        const response = await cloudinary.uploader.upload(
-            localFilePath,
-            {
-                resource_type: "auto"
-            }
-        )
-        console.log("File uploaded on cloudinary. File src: " + response.url)
-        // once the file is uploaded, we would like to delete it from our server
-        fs.unlinkSync(localFilePath);
-        return response
+// const uploadOnCloudinary = async (localFilePath) => {
+//     try {
+//         // console.log("Cloudinary Confiq:",{
+//         //     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//         //     api_key: process.env.CLOUDINARY_API_KEY,
+//         //     api_secret: process.env.CLOUDINARY_API_SECRET
+//         // })
+//         if(!localFilePath) return null
+//         const response = await cloudinary.uploader.upload(
+//             localFilePath,
+//             {
+//                 resource_type: "auto"
+//             }
+//         )
+//         console.log("File uploaded on cloudinary. File src: " + response.url)
+//         // once the file is uploaded, we would like to delete it from our server
+//         fs.unlinkSync(localFilePath);
+//         return response
         
-    } catch (error) {
-        console.log("Error on Cloudinary ", error)
-        fs.unlinkSync(localFilePath);
-        return null
-    }
-}
+//     } catch (error) {
+//         console.log("Error on Cloudinary ", error)
+//         fs.unlinkSync(localFilePath);
+//         return null
+//     }
+// }
 
-const deleteFromCloudinary = async (publicId) => {
-    try {
-        const result = await cloudinary.uploader.destroy(publicId)
-        console.log("Deleted from cloudinary. Public id", publicId)
-    } catch (error) {
-        console.log("Error deleting from cloudinary", error);
-        return null
-    }
-}
+// const deleteFromCloudinary = async (publicId) => {
+//     try {
+//         const result = await cloudinary.uploader.destroy(publicId)
+//         console.log("Deleted from cloudinary. Public id", publicId)
+//     } catch (error) {
+//         console.log("Error deleting from cloudinary", error);
+//         return null
+//     }
+// }
 
-export { uploadOnCloudinary, deleteFromCloudinary }
+// export { uploadOnCloudinary, deleteFromCloudinary }
